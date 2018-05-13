00ceb8d8e424eb63f81cc737831565fb5dcdc50f
Pavel Viznitsa <viznitsap@gmail.com>
2018-05-13 23:11:27 +0300

    search for active dots

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 var slide = document.querySelectorAll('.slide');
+var prev = document.querySelector('.prev-slide');
+var next = document.querySelector('.next-slide');
+
 function createSlides() {
+  var sliderLength = 1;
   slide.forEach(function(el, idx) {
     el.style.backgroundImage = 'url(../img/work-' + (idx + 1) + '.jpg)';
     el.style.backgroundSize = 'cover';
+    sliderLength = idx + 1;
   });
+  return sliderLength;
 }
 
-function renderDots() {
+function renderDots(params) {
   var dots = document.querySelector('#dots');
 
   for (var i = 0, max = slide.length; i < max; i += 1) {
@@ -15,8 +21,29 @@ function renderDots() {
     dots.appendChild(dot);
   }
   dots.firstChild.nextSibling.classList.add('active');
-  console.log('dots.firstChild.nextSibling ', dots.firstChild.nextSibling);
 }
 
-createSlides();
-renderDots();
+function prevSlide(params) {
+  console.log('prevSlide click');
+}
+function nextSlide(params) {
+  console.log('nextSlide click');
+  var dot = document.querySelectorAll('.dot');
+  for (let i = 0; i < dot.length; i++) {
+    const element = dot[i];
+    console.log(element.classList.contains('active'));
+  }
+}
+
+function eventListeners() {
+  prev.addEventListener('click', prevSlide);
+  next.addEventListener('click', nextSlide);
+}
+
+function main() {
+  createSlides();
+  renderDots();
+  eventListeners();
+}
+
+main();
