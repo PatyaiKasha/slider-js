0e848ec9fa9ceddb9e4f7aff484f65709fedb687
Pavel Viznitsa <viznitsap@gmail.com>
2018-05-13 22:45:42 +0300

    added dots to slider-footer

--- js/index.js
+++ js/index.js
@@ -1,25 +1,21 @@
+var slide = document.querySelectorAll('.slide');
 function createSlides() {
-  var slide = document.querySelectorAll('.slide');
-  for (var i = 0, max = slide.length; i <= max; i += 1) {
-    var element = slide[i];
-    element.style.backgroundImage = 'url(../img/work-' + (i + 1) + '.jpg)';
-    element.style.backgroundSize = 'cover';
-  }
+  slide.forEach(function(el, idx) {
+    el.style.backgroundImage = 'url(../img/work-' + (idx + 1) + '.jpg)';
+    el.style.backgroundSize = 'cover';
+  });
 }
 
 function renderDots() {
   var dots = document.querySelector('#dots');
-  console.log('dots ', dots);
-  var dot = document.createElement('div').className('dot');
-  dots.insertAdjacentHTML('afterbegin', dot);
 
-  //   var dot = document.createElement('div').className('dot');
-
-  //   for (var i = 0, max = quantity.length; i <= max; i += 1) {
-  //     // var element = dot[i];
-  //     out += dot;
-  //   }
-  //   dots.appendChild(dot);
+  for (var i = 0, max = slide.length; i < max; i += 1) {
+    var dot = document.createElement('div');
+    dot.className = 'dot';
+    dots.appendChild(dot);
+  }
+  dots.firstChild.nextSibling.classList.add('active');
+  console.log('dots.firstChild.nextSibling ', dots.firstChild.nextSibling);
 }
 
 createSlides();
